be1311368e018585bf436f1697cad8539f71072c
Henry Heino <personalizedrefrigerator@gmail.com>
2023-09-08 03:19:13 -0700

    Accessibility bug report for react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import MainScreen from './MainScreen';
-import { MD3LightTheme, PaperProvider } from 'react-native-paper';
+import {MD3LightTheme, PaperProvider} from 'react-native-paper';
 
 const App = () => {
 	return (
 		<PaperProvider theme={MD3LightTheme}>
 			<MainScreen />
 		</PaperProvider>
-   );
-}
+	);
+};
 
 export default App;
--- MainScreen.tsx
+++ MainScreen.tsx
@@ -1,63 +1,72 @@
-
 import React from 'react';
-import { useState, useCallback } from 'react';
+import {useState, useCallback} from 'react';
 
-import { View, Text, StyleSheet } from 'react-native';
-import { Portal, FAB } from 'react-native-paper';
-
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {Portal, FAB, Checkbox} from 'react-native-paper';
 
 const MainScreen = () => {
-	const [ open, setOpen ] = useState(false);
-
-	const onStateChange = useCallback(() => {
-		setOpen(!open);
-	}, [ open ]);
-
-	const [ counter, setCounter ] = useState(0);
+	// Counter state
+	const [counter, setCounter] = useState(0);
 	const onAdd = useCallback(() => {
 		setCounter(counter + 1);
-	}, [ counter ]);
+	}, [counter]);
 	const onSubtract = useCallback(() => {
 		setCounter(counter - 1);
-	}, [ counter ]);
+	}, [counter]);
+
+	// FAB state.
+	// setShowFAB: Whether to include the FAB in the rendered output.
+	const [showFAB, setShowFAB] = useState(true);
+
+	const [open, setOpen] = useState(false);
+
+	const fab = (
+		<Portal>
+			<FAB.Group
+				open={open}
+				visible
+				accessibilityLabel={'Counter actions'}
+				icon={'plus-minus-variant'}
+				actions={[
+					{icon: 'plus-thick', label: 'Add', onPress: onAdd},
+					{icon: 'minus', label: 'Subtract', onPress: onSubtract},
+				]}
+				onStateChange={() => setOpen(!open)}
+			/>
+		</Portal>
+	);
 
 	return (
 		<View>
-			<View>
-				<Text style={styles.description}>
-					This sample app demonstrates an accessibility
-					issue in react-native-paper. Enable TalkBack or
-					VoiceOver and attempt to move accessibility focus
-					to this element by touching it.
-				</Text>
-			</View>
-			<View>
-				<Text style={styles.counter}>{counter}</Text>
-			</View>
-			<Portal>
-				<FAB.Group
-					open={open}
-					visible
-					accessibilityLabel={'Counter actions'}
-					icon={'plus-minus-variant'}
-					actions={[
-						{ icon: 'plus-thick', label: 'Add', onPress: onAdd },
-						{ icon: 'minus', label: 'Subtract', onPress: onSubtract }
-					]}
-					onStateChange={onStateChange}
-				/>
-			</Portal>
+			<SafeAreaView>
+				<View>
+					<Text style={styles.description}>
+						This sample app demonstrates an accessibility issue in
+						react-native-paper. Enable TalkBack or VoiceOver and attempt to move
+						accessibility focus to this element by touching it.
+					</Text>
+					<Text style={styles.counter}>{counter}</Text>
+
+					<Checkbox.Item
+						label="Show FAB"
+						status={showFAB ? 'checked' : 'unchecked'}
+						onPress={() => setShowFAB(!showFAB)}
+					/>
+				</View>
+			</SafeAreaView>
+			{showFAB ? fab : null}
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
 	description: {
-		fontSize: 1.2,
+		fontSize: 18,
 	},
 
 	counter: {
-		fontSize: 3,
+		fontSize: 48,
+		textAlign: 'center',
 	},
 });
 
