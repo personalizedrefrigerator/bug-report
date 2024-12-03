4d8974d73c254d572dfe00a77f73ac92ad17e8cb
Henry Heino <personalizedrefrigerator@gmail.com>
2024-12-02 16:41:50 -0800

    Demonstrate low contrast bug

--- project/lib.ts
+++ project/lib.ts
@@ -6,28 +6,15 @@
  * @packageDocumentation
  */
 
-/** A constant. Links work here: {@link Bar} */
-export const var1 = 4;
-
-/** Another variable -- export multiple so that several icons are present in the sidebar. */
-export const var2 = 5;
-
-/** Another variable -- export multiple so that several icons are present in the sidebar. */
-export const var3 = 6;
-
-/** Another variable -- export multiple so that several icons are present in the sidebar. */
-export const var4 = 7;
-
-/** Another variable -- export multiple so that several icons are present in the sidebar. */
-export const var5 = 8;
-
-/** Another variable -- export multiple so that several icons are present in the sidebar. */
-export const var6 = 9;
-
-/** Another variable -- export multiple so that several icons are present in the sidebar. */
-export const var7 = 10;
 
 /** A class */
-export class Bar {
+export class Example {
+	/** When rendered by TypeDoc in the default light theme, this has low contrast. */
+	public lowContrast() {
+		return 3;
+	}
 
+	get alsoLowContrast() {
+		return 4;
+	}
 }
--- theme/index.ts
+++
@@ -1,6 +0,0 @@
-import { Application } from 'typedoc';
-import { CustomTheme } from './CustomTheme';
-
-export const load = (app: Application) => {
-	app.renderer.defineTheme('my-theme', CustomTheme);
-};
